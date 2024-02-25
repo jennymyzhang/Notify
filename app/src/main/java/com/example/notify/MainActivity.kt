73118a41b57e54bbb045d31263c9086877f1220d
jennymyzhang <j2466zha@uwaterloo.ca>
2024-02-24 23:04:17 -0500

    Main activity update

---
+++ app/src/main/java/com/example/notify/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.notify
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import com.example.notify.ui.Navigation
+import com.example.notify.ui.theme.ComposeLoginScreenInitTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            ComposeLoginScreenInitTheme {
+                Navigation(navController)
+            }
+        }
+    }
+
+}
